323e8ddf16e3905b2575d04ebbcbc503577158c6
billymlelatobur17 <ulansarni@outlook.com>
2023-01-13 10:34:50 +0700

    setup spatie

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    public function run()
+    {
+        $admin = User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('password'),
+        ]);
+
+        $admin->assignRole('admin');
+    }
+}
