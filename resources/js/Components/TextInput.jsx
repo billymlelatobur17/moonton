b48ed3cbdd63075a1571765554fc1d9bd498bd9f
billymlelatobur17 <ulansarni@outlook.com>
2023-01-14 09:13:34 +0700

    Slicing Login dan Register

--- resources/js/Components/TextInput.jsx
+++ resources/js/Components/TextInput.jsx
@@ -1,7 +1,24 @@
 import { forwardRef, useEffect, useRef } from 'react';
+import PropTypes from "prop-types";
+import {Input} from "postcss";
+
+Input.propTypes = {
+    type:PropTypes.oneOf(['text', 'email', 'password','number','file']),
+    name:PropTypes.string,
+    value:PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    defaultValue: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    className: PropTypes.string,
+    variant: PropTypes.oneOf(['primary', 'error', 'primary-outline']),
+    autoComplete: PropTypes.string,
+    required: PropTypes.bool,
+    isFocused: PropTypes.bool,
+    handleChange: PropTypes.func,
+    placeholder: PropTypes.string,
+
+}
 
 export default forwardRef(function TextInput(
-    { type = 'text', name, id, value, className, autoComplete, required, isFocused, handleChange },
+    { type = 'text', name, id, value, className, autoComplete, required, isFocused, handleChange,defaultValue, variant = "primary", placeholder, isError },
     ref
 ) {
     const input = ref ? ref : useRef();
@@ -19,14 +36,15 @@ export default forwardRef(function TextInput(
                 name={name}
                 id={id}
                 value={value}
+                defaultValue={defaultValue}
                 className={
-                    `border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm ` +
-                    className
+                    `rounded-2xl bg-form-bg py-[13px] px-7 w-full ${isError && "input-error"} input-${variant} ${className}`
                 }
                 ref={input}
                 autoComplete={autoComplete}
                 required={required}
                 onChange={(e) => handleChange(e)}
+                placeholder={placeholder}
             />
         </div>
     );
--- routes/web.php
+++ routes/web.php
@@ -26,14 +26,26 @@ Route::get('/', function () {
     ]);
 });
 
+Route::redirect('/','/prototype/login');
+
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::prefix('prototype')->name('prototype.')->group(function () {
+    route::get('/login',function () {
+        return Inertia::render('Prototype/Login');
+    })->name('login');
+    route::get('/register',function () {
+        return Inertia::render('Prototype/Register');
+    })->name('register');
 });
+
+
+//Route::middleware('auth')->group(function () {
+//    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+//});
 
 require __DIR__.'/auth.php';
